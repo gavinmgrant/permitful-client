4422f36637c8fbdd038ab23e47ca3c0e6eda0f23
Gavin Grant <gavingrant@gmail.com>
2020-09-18 11:12:03 -0700

    revised copy in registration and log in and added favicon

--- src/components/LogInForm/LogInForm.js
+++ src/components/LogInForm/LogInForm.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LogInForm.css';
 
 export default function LogInForm() {
     return (
         <section className="login-container">
             <h2>Log In</h2>
+            <p>Welcome back! Please log in below to access your favorites. If you'd like to register for an account at Permitful,{' '}
+                <span>
+                    <Link to='/register'>register here</Link>
+                </span>.
+            </p>
             <form>
                 <section className="login-form">
                     <div>
