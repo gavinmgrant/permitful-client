8b8e79c2b8691d91d51d41b6a9cc196d9ed1db1f
Gavin Grant <gavingrant@gmail.com>
2020-09-17 17:20:38 -0700

    updated landing page copy

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -34,7 +34,7 @@ class Landing extends Component {
 
         <section className='landing-sections'>
           <h2>Permit details</h2>
-          <p>Once you have found a property, you can dig into its details. The property details lists any building permits and tells you their status, such as open, completed, or expired. A description of each permit will tell you the scope of work.</p>
+          <p>Once you have found a property, you can dig into its details. The property details lists any building permits on file and provides you their number, status, and a brief description.</p>
         </section>
 
         <section className='landing-sections'>
